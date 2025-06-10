396cdebad16c3ae4e6af5e8a9d31f66fa20f5e6b
Carl <carl.rippon@googlemail.com>
2025-06-10 04:38:55 +0100

    Chapter 10 'ue client' fix

--- Chapter10/context/src/app/layout.tsx
+++ Chapter10/context/src/app/layout.tsx
@@ -29,7 +29,6 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable}`}
       >
-        {/* Add provider */}
         <UserProvider>{children}</UserProvider>
       </body>
     </html>
--- Chapter10/context/src/components/Content.tsx
+++ Chapter10/context/src/components/Content.tsx
@@ -1,3 +1,4 @@
+'use client';
 import { use } from 'react';
 import { UserContext } from '@/state/UserContext';
 
